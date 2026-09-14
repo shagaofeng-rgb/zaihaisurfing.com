82ea59ac4d43f4c29571bb3bc7b480f77cbe38a5
shagaofeng-rgb <shagaofeng@gmail.com>
2026-09-14 16:26:42 +0800

    Update company map to owner supplied location

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -19,8 +19,8 @@ export default async function ContactPage({params}: {params: Promise<{locale: Lo
   const copy = uiCopy[locale].contactPage;
   const company = copy.company;
   const address = 'Room 110, 1st Floor, Building 2, Qushidai Future Building, Kecheng District, Quzhou, Zhejiang Province, China';
-  const mapSrc = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d738.4079128143358!2d118.8398291714169!3d28.96554554896867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sus!4v1780549851421!5m2!1sen!2sus';
-  const mapUrl = 'https://www.google.com/maps/search/?api=1&query=28.96554554896867,118.8398291714169';
+  const mapSrc = `https://maps.google.com/maps?q=28.965204,118.839750&z=17&hl=${locale}&output=embed`;
+  const mapUrl = 'https://maps.app.goo.gl/P1YyVHoCdGBd9ef37';
   const trustBlocks = [
     'Reply within 24 hours on business days',
     'Product videos available on request',
